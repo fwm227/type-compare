33358282d00b306ace68de34d66e52bdf0e49da6
reming <927627286@qq.com>
2019-06-08 16:23:57 +0800

    build: add rollup for build project

---
+++ bundle.js
@@ -0,0 +1,17 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, global.compareType = factory());
+}(this, function () { 'use strict';
+
+  function typeCheck(val, checkType) {
+    var type = typeof val;
+    if (val === null) type = 'null';
+    else if (Array.isArray(val)) type = val.constructor.name;
+    else if (type === 'object') type = `object.${Object.prototype.toString.call(val).slice(8, -1)}`;
+    return type.toLowerCase() === checkType;
+  }
+
+  return typeCheck;
+
+}));
