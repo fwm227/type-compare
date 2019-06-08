d904583c91bf0a36774eda38ab866b63150b9e94
reming <927627286@qq.com>
2019-06-08 17:44:58 +0800

    test: update jest demo

--- _tests_/build.test.js
+++ _tests_/build.test.js
@@ -1,6 +1,7 @@
 const typeCompare = require('../bundle.js');
 
 test('type-compare test', () => {
+  // true test
   expect(typeCompare(null, 'null')).toBeTruthy();
   expect(typeCompare(undefined, 'undefined')).toBeTruthy();
   expect(typeCompare(true, 'boolean')).toBeTruthy();
@@ -13,4 +14,16 @@ test('type-compare test', () => {
   expect(typeCompare(new String(), 'object.string')).toBeTruthy();
   expect(typeCompare(new Set(), 'object.set')).toBeTruthy();
   expect(typeCompare(new Map(), 'object.map')).toBeTruthy();
+  // fase test
+  expect(typeCompare(undefined, 'null')).toBeFalsy();
+  expect(typeCompare(null, 'undefined')).toBeFalsy();
+  expect(typeCompare('true', 'boolean')).toBeFalsy();
+  expect(typeCompare('123', 'number')).toBeFalsy();
+  expect(typeCompare(123, 'string')).toBeFalsy();
+  expect(typeCompare(new Object(), 'array')).toBeFalsy();
+  expect(typeCompare(new Array(), 'object')).toBeFalsy();
+  expect(typeCompare(new Number('123'), 'object.string')).toBeFalsy();
+  expect(typeCompare(new String(123), 'object.number')).toBeFalsy();
+  expect(typeCompare(new Set(), 'object.object')).toBeFalsy();
+  expect(typeCompare(new Map(), 'object.object')).toBeFalsy();
 })
